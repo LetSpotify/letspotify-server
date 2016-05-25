5a13d4df14f398d2f10b5c382a4b5e64376f21dc
Kevin Chen <me@kevchentw.com>
2016-05-25 16:07:15 +0800

    add more info in chat msg

--- handlers/chats.py
+++ handlers/chats.py
@@ -1,6 +1,7 @@
 import logging
 import json
 import tornado
+import time
 
 from .base import BaseHandler
 from .base import Service
@@ -17,15 +18,20 @@ class ChatHandler(BaseHandler):
         ]
         paras = self.get_parameters(para_q)
         yield from self.is_room_master_or_subscriber(paras)
-        room, success, msg = yield from Service.rooms.get_info(paras)
+        room, success_room, msg_room = yield from Service.rooms.get_info(paras)
+        user, success_user, msg_user = yield from Service.users.get_user({'fid': self.get_user()})
+        msg = {
+            "type": "chat",
+            "data": {
+                'msg': paras['msg'],
+                'user': user,
+                'timestamp': int(time.time())
+            }
+        }
         if room:
             clients = Service.ws_rooms.get_clients(paras['rid'])
             if clients:
                 for client in clients:
-                    msg = {
-                        "type": "chat",
-                        "msg": paras['msg']
-                    }
                     client.write_message(json.dumps(msg))
                     return self.api_response({}, True, "")
             else:
